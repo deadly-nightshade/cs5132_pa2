8364b8198919cc6bed56ab93ac883d23eb7d8f58
robotguy999 <aydenang1234@gmail.com>
2023-10-09 22:32:56 +0800

    Basic TableView and updateTable()functionality

--- src/main/java/me/squiddy/pa2/MainController.java
+++ src/main/java/me/squiddy/pa2/MainController.java
@@ -16,7 +16,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
 public class MainController {
     // initialise main scene
@@ -45,9 +44,6 @@ public class MainController {
         info.setCellValueFactory(new PropertyValueFactory<>("info"));
         priority.setCellValueFactory(new PropertyValueFactory<>("priority"));
         table.itemsProperty().bind(patientListProperty);
-        patientList.add(new PatientEntry(1, new Patient("Ayden", 17, LocalDate.now().minusDays(5), LocalDate.now().plusDays(2))));
-        patientList.add(new PatientEntry(1, new Patient("Kabir", 18, LocalDate.now().minusDays(4), LocalDate.now().plusDays(1))));
-        System.out.println(table.getItems());
         table.refresh();
     }
 
