22a9ce820d4e33dff1053917f8c6d958be8b70c9
pgot <neonx2006@gmail.com>
2023-10-09 11:26:45 +0800

    (feat): Dequeue patient

--- src/main/java/me/squiddy/pa2/MainController.java
+++ src/main/java/me/squiddy/pa2/MainController.java
@@ -1,9 +1,11 @@
 package me.squiddy.pa2;
 
 import PQueue.PQueueViewable;
+import PQueue.Pair;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
@@ -38,14 +40,21 @@ public class MainController {
     }
 
     @FXML
-    void editPatient() {
-        // go to patient page
-        // update table
-    }
-
-    @FXML
-    void deletePatient() {
-        // delete
-        // update table
+    void dequeuePatient(){
+        if(pq.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("No patients left to dequeue!");
+            alert.showAndWait();
+        }
+        else{
+            Patient p = pq.dequeue().getLeft();
+            String message = "Name: " + p.getName() + "\nAge: " + p.getAge() + "\nRegistration Date: " + p.getRegistrationDate() + "\nDeath Date: " + p.getDeathDate();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Patient removed");
+            alert.setHeaderText(message);
+            alert.showAndWait();
+            updateTable();
+        }
     }
 }
